f0b6cf6cd3d6e7bfa8721a32f415501998ecfe3c
Jandre-Jooste77 <576340@student.belgiumcampus.ac.za>
2021-03-01 17:39:05 +0200

    Jandre Wedding Code

--- BusinessLogicLayer/Wedding.java
+++ BusinessLogicLayer/Wedding.java
@@ -1,6 +1,8 @@
 package BusinessLogicLayer;
 import java.util.*;
 
+import jdk.javadoc.internal.tool.Main;
+
 public class Wedding implements Event{
     String eventType = "Wedding";
     String eventDateandTime;
@@ -8,8 +10,9 @@ public class Wedding implements Event{
     int eventNumPeople;
     
     //menu
+    
     String starters;
-    String mainCourse;
+    String Main;
     String Desert;
 
 
@@ -17,28 +20,45 @@ public class Wedding implements Event{
 	@Override
 	public String[] getEventDetails() {
 		
+        //Jandre Wedding change
+
         Scanner scn = new Scanner(System.in);
         String[] weddingDeets = new String[7];
+        weddingDeets[0]= eventType;
 
         System.out.println("When(Date & Time)");
         eventDateandTime = scn.nextLine();
+        weddingDeets[1] = eventDateandTime;
 
         System.out.println("WhereAddress)");
         eventAddress = scn.nextLine();
+        weddingDeets[2] = eventAddress;
+
 
         System.out.println("How many people?");
         eventNumPeople = scn.nextInt();
+        weddingDeets[3] = String.valueOf(eventNumPeople).toString();
+
 
 
         System.out.println("==================================Menu:=============================");
         System.out.println("What starters would you like?");
         starters = scn.nextLine();
+        weddingDeets[4] = starters;
+
 
         System.out.println("What would you like for the main course?");
-        mainCourse = scn.nextLine();
+        Main = scn.nextLine();
+        weddingDeets[5] = Main;
+
 
         System.out.println("What would you like for desert?");
         Desert = scn.nextLine();
+        weddingDeets[6] = Desert;
+
+
+
+
 
         scn.close();
 
