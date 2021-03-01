df9981833c927a0fda6a2eb32b26be260ef59db4
Jandre-Jooste77 <576340@student.belgiumcampus.ac.za>
2021-03-01 16:56:21 +0200

    Jandre Funeral

--- BusinessLogicLayer/Funeral.java
+++ BusinessLogicLayer/Funeral.java
@@ -5,9 +5,38 @@ import java.util.*;
 
 public class Funeral implements  Event{
 
-    //This is a funeral though
-    //this is my comment
-    //this is my third comment
+    //Jandre Funeral class
+
+    String eventType = "Funeral";
+    String eventDateandTime;
+    String eventAddress;
+    int eventNumPeople;
     
+    //menu
+    String FuneralBouquet;
     
+
+
+
+	@Override
+	public void getEventDetails() {
+		
+        Scanner scn = new Scanner(System.in);
+        System.out.println("When(Date & Time)");
+        eventDateandTime = scn.nextLine();
+
+        System.out.println("WhereAddress)");
+        eventAddress = scn.nextLine();
+
+        System.out.println("How many people?");
+        eventNumPeople = scn.nextInt();
+
+
+        System.out.println("==================================Menu:=============================");
+        System.out.println("What starters would you like?");
+        FuneralBouquet = scn.nextLine();
+
+        scn.close();
+    }
 }
+
