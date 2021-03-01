93fbcc3c0c3888677478d7dc068ac8ce584e9f0d
FIRST_NAME LAST_NAME <576341@student.belgiumcampus.ac.za>
2021-03-01 17:36:05 +0200

    Birthday class indexes

--- BusinessLogicLayer/BirthDay.java
+++ BusinessLogicLayer/BirthDay.java
@@ -21,35 +21,42 @@ public class BirthDay implements Event{
         Scanner scn = new Scanner(System.in);
         String[] birthdayDeets = new String[7];
 
+        
 
         System.out.println("When(Date & Time)");
         eventDateandTime = scn.nextLine();
+        birthdayDeets[0] = eventDateandTime;
 
         System.out.println("WhereAddress)");
         eventAddress = scn.nextLine();
+        birthdayDeets[1] = eventAddress;
 
         System.out.println("How many people?");
         eventNumPeople = scn.nextInt();
+        birthdayDeets[2] = String.valueOf(eventNumPeople);
 
         System.out.println("Do you have a theme?(Yes/No");
         eventTheme = scn.nextLine();
+        birthdayDeets[3] = eventTheme;
 
 
         System.out.println("==================================Menu:=============================");
         
         System.out.println("What starters would you like?");
         starters = scn.nextLine();
+        birthdayDeets[4] = starters;
 
         System.out.println("What would you like for the main course?");
         mainCourse = scn.nextLine();
+        birthdayDeets[5] = mainCourse;
 
         System.out.println("What would you like for desert?");
         Desert = scn.nextLine();
+        birthdayDeets[6] = Desert;
 
         scn.close();
 
         return birthdayDeets;
-
        
     }
 }
