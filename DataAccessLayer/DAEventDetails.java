25d9c34f153ea7d12bbb6a2b033cf828e48af88d
Johan Strauss <576346@student.belgiumcampus.ac.za>
2021-03-01 17:48:06 +0200

    Wedding combined string & if statement

--- DataAccessLayer/DAEventDetails.java
+++ DataAccessLayer/DAEventDetails.java
@@ -1,18 +1,35 @@
 package DataAccessLayer;
-
-import java.io.File;
+import java.io.*;
 
 public class DAEventDetails {
 
-    public void DAEventDetail(String[] stArr){
+    public void DAEventDetail(String[] stArr) throws IOException {
 
         
-
-        File file = new File("ClientDetails.txt");
+        File file = new File("EventDetails.txt");
         String path = file.getAbsolutePath();
         
         FileWriter fileWriter = new FileWriter(path,true);
         BufferedWriter bwriter = new BufferedWriter(fileWriter);
+
+
+        if (stArr[0] == "Wedding") {
+
+            String CombinedWedding = stArr[0] + "," + stArr[1] + "," + stArr[2] +"," + stArr[3] +", Menu: "+ stArr[4] +"," + stArr[5] +"," + stArr[6] +" \n";
+            bwriter.write(CombinedWedding);
+
+        } else if(stArr[0] == "BirthDay") {
+            
+        }else if (stArr[0] == "Funeral"){
+
+        }else {
+
+
+        }
+
+        bwriter.close();
+
+       
 
 
         
