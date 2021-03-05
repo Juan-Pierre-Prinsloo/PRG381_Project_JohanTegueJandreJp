de0214867ec838abbcda9989163c0021fc44a5bb
Johan Strauss <576346@student.belgiumcampus.ac.za>
2021-03-05 17:11:15 +0200

    Bruh

--- BusinessLogicLayer/LoginFactory.java
+++ BusinessLogicLayer/LoginFactory.java
@@ -1,12 +1,20 @@
 package BusinessLogicLayer;
 
 import java.io.IOException;
+import java.util.*;
 
 import DataAccessLayer.DAClientDetails;
+import PresentationLayer.Clientview;
 
 public class LoginFactory {
     Client client = new Client();
     DAClientDetails daclientdetails = new DAClientDetails();
+
+    PlanEventFactory planning = new PlanEventFactory();
+    Clientview cl = new Clientview();
+    Scanner scner = new Scanner(System.in);  
+
+    int eventChoice = 0;
    
 
     public Client GetEntry(int SignUp) throws IOException {
@@ -15,9 +23,15 @@ public class LoginFactory {
             switch (SignUp) {
                 case 1:
                     daclientdetails.getClientDetails();
+                    eventChoice = scner.nextInt();
+                    cl.displayPlanMenu();
+
+                    planning.PlanMenu(eventChoice);
+                    
                     break;
                 case 2:
                     System.out.println("-Weddings \n -Birthdays \n -Funerals \n -Other events you may want.");
+                    break;
                 case 3:
                     System.exit(0);
                     break;
--- PresentationLayer/ClientView.java
+++ PresentationLayer/ClientView.java
@@ -1,37 +1,64 @@
 package PresentationLayer;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 import BusinessLogicLayer.LoginFactory;
+import BusinessLogicLayer.PlanEventFactory;
 
 public class Clientview {
     public static void main(String[] args) throws IOException {
 
         LoginFactory factory = new LoginFactory();
-        boolean flag = true;
+        PlanEventFactory planning = new PlanEventFactory();
+        //boolean flag = true;
 
-        while (flag == true) {
-            System.out.println("====================Welcome to Rachael's Event planning===================== \n");
-            Scanner scner = new Scanner(System.in);
+        Scanner scner = new Scanner(System.in);
+        
+       
+
+        int eventChoice = 0;
+        int choice = 0;
+
+
+        while (true) {
+            
+            /*displaySignInMenu();
+    
+            choice = extracted(scner);
+            
+            factory.GetEntry(choice);
+
+            displayPlanMenu();
+
+            eventChoice = extracted(scner);
+            
+            planning.PlanMenu(eventChoice);
+
+
+            if (choice == 1) {
+               
+               
+
+
+            }*/
+    
+        }
+    }
+
+    
+
+    public static void displayPlanMenu(){
+        System.out.println("=========Event Planning================");
+        System.out.println("Choose the type of event:");
+        System.out.println("1.Wedding \n2.Birthday \n3.Funeral \n4.Other");
+    }
+    public static void displaySignInMenu(){
+        System.out.println("====================Welcome to Rachael's Event planning===================== \n");
     
             System.out.println("Dou you want to Sign Up?");
-    
             System.out.println("Press: \n 1. Sign Up \n 2. List of Events we offer. \n 3. Exit" );
-    
-            int choice = scner.nextInt();
-            
-            if (choice == 1) {
-                flag = false;
-            }
-    
-            factory.GetEntry(choice);
-    
-            scner.close();
-        }
-        
-      
-
-        
     }
+
+   
 }
