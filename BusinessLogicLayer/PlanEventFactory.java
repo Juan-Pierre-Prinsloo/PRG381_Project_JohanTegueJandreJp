34068d54839d9269983c5246d06b136345dee4f2
Johan Strauss <576346@student.belgiumcampus.ac.za>
2021-03-05 17:11:02 +0200

    Weni

--- BusinessLogicLayer/PlanEventFactory.java
+++ BusinessLogicLayer/PlanEventFactory.java
@@ -1,25 +1,41 @@
 package BusinessLogicLayer;
 
+import java.io.IOException;
+import java.util.*;
+
+import DataAccessLayer.DAEventDetails;
+
 public class PlanEventFactory {
     Event event;
+    DAEventDetails devent = new DAEventDetails();
+    
 
-    public Event PlanMenu(int choice){
+    public Event PlanMenu(int eventChoice) throws IOException {
+        /*Scanner scn = new Scanner(System.in);
+        int choice = 0;
+        choice = scn.nextInt();*/
+        
 
-        switch (choice) {
+
+        switch (eventChoice) {
             case 1://wedding
                 event = new Wedding();
+                devent.DAEventDetail(event.getEventDetails());
                 break;
         
             case 2://bifday
                 event = new BirthDay();
+                devent.DAEventDetail(event.getEventDetails());
                 break;
         
             case 3://funeral
                 event = new Funeral();
+                devent.DAEventDetail(event.getEventDetails());
                 break;
         
             case 4://other
                 event = new OtherEvent();
+                devent.DAEventDetail(event.getEventDetails());
                 break;
         
             default:
